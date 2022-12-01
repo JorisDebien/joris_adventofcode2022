5b8c3f84a19d01d9fa4f3c09a78caf8cc5c4f238
JorisDebien <jorisdebien@gmail.com>
2022-12-01 13:26:28 +0100

    Finished solution

--- 2022_1/2022_1.py
+++ 2022_1/2022_1.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+elfArray = []
+
+with open(r"C:\Users\joris\Documents\GitHub\joris_adventofcode2022\2022_1\2022_1_input.txt") as file:
+    tempSum = 0
+    for i in file:
+        if i != "\n":
+            tempSum += int(i)           
+        elif i == "\n":
+            elfArray.append(tempSum)
+            tempSum = 0
+
+print(max(elfArray))
